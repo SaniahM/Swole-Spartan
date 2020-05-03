2f11b383b64044c667e5475b497ba018a6793e9f
SaniahM <57219263+SaniahM@users.noreply.github.com>
2020-05-04 00:44:49 +0500

    Add files via upload

--- lib/post_review.dart
+++ lib/post_review.dart
@@ -2,13 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PostReview{
 
-  final String reviewDocId;
-  PostReview(this.reviewDocId);
-
   final CollectionReference reviews = Firestore.instance.collection('reviews');
 
   Future newReview(String productId, String name, String reviewText) async {
-    return await reviews.document(reviewDocId).setData({
+    return await reviews.document().setData({
       'product_id': productId,
       'user_name': name,
       'review' : reviewText,
