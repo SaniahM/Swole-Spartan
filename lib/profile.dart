74eb87883aeb3068508b62f3a2ee9953cad5715b
Ali Bukhari <alibukhari6728@gmail.com>
2020-05-06 01:41:24 +0500

    basic profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,56 +1,45 @@
-// import 'package:flutter/material.dart';
-// import 'appbar.dart';
-// import 'footnavbar.dart';
-// import 'properties.dart';
 
-// class ProfilePage extends StatefulWidget{
-//   @override
-//   _ProfilePage createState() => _ProfilePage();
-// }
-
-// class _ProfilePage extends State<ProfilePage>{
-//   int _currentIndex = 6;
-//   @override
-//   Widget build(BuildContext context){
-
-//     return Scaffold(
-      
-//       backgroundColor: Colors.grey[getColor()[0]],
-
-//       appBar: topbar(context, getColor()[0], getColor()[1]),
-
-//       body: Card(
-//               color: Colors.white,
-//               elevation: 10,
-//               margin: EdgeInsets.only(top:8),
-//               shape: RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(40))),
-
-// //////////////////////////////////////////////////////////////////////////////////////////
-// /////CHANGE ONLY THIS PART TO FIT ANY SCREEN INTO THE COMMON ITEMS TEMPLATE/////////////
-            
-//               child: Column(
-//                 children:  
-//                   [
-//                     Padding(padding: EdgeInsets.all(196.5)),
-//                     Padding(padding: EdgeInsets.all(100)),
-//                   ]  
-               
-//                 )
-
-// ///////////////////////////////////////////////////////////////////////////////////////////////
-// ///////////////////////////////////////////////////////////////////////////////////////////////////      
-//       ),
-
-//       bottomNavigationBar: footBar(_currentIndex, (index){
-//             setState(() {
-//               _currentIndex = index;
-//             });
-//           }), 
-
-//       );
-//   }
-// }
-
-// //Myapp
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'appbar.dart';
+import 'footnavbar.dart';
+import 'properties.dart';
+import 'screens.dart';
 
 
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+
+class _ProfilePageState extends State<ProfilePage> {
+  int _currentIndex = 12;
+  int _sectionIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    
+     return Scaffold(
+      backgroundColor: Colors.grey[getColor()[0]],
+      appBar: topbar(context, getColor()[0], getColor()[1], _sectionIndex),
+      body: Card(
+        color: Colors.white,
+        elevation: 10,
+        margin: EdgeInsets.only(top: 8),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(40))),
+
+//////////////////////////////////////////////////////////////////////////////////////////
+/////CHANGE ONLY THIS PART TO FIT ANY SCREEN INTO THE COMMON ITEMS TEMPLATE/////////////
+
+        child: screens()[_currentIndex],
+      ),
+      bottomNavigationBar: footBar(_sectionIndex, (index){
+            setState(() {
+              _currentIndex = index;
+              _sectionIndex = index;
+            });
+          }),
+    );
+  }
+}
---
+++ lib/profile_sub.dart
@@ -0,0 +1,292 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'shopping_list.dart';
+import 'user.dart';
+
+import 'package:provider/provider.dart';
+
+
+
+class ProfilePageSub extends StatefulWidget {
+  @override
+  _ProfilePageStateSub createState() => _ProfilePageStateSub();
+}
+
+
+class _ProfilePageStateSub extends State<ProfilePageSub> {
+
+
+
+@override
+Widget build(BuildContext context) {
+
+final user = Provider.of<User>(context);
+
+return StreamBuilder<UserData>(
+    stream: DatabaseService(uid: user.uid).userData,
+    
+     builder:(context,snapshot){ 
+       
+       UserData userdata = snapshot.data;
+
+        return Container(
+            height: MediaQuery
+                .of(context)
+                .size
+                .height,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
+
+            child: Wrap(
+              children: <Widget>[
+                //CHECKOUT TITLE
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    padding: EdgeInsets.fromLTRB(0, 3.0, 0.0, 0.0),
+                    //width: 10,
+                    height: 28,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: Colors.white,
+                    ),
+                    child: Text(
+                      'Account Profile',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: ssFont,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ),
+                ),
+
+                //DELIVERY DETAILS
+
+                
+                Container(
+                  height: 300,
+                  //color:Colors.pink,
+                  margin: EdgeInsets.fromLTRB(60, 20, 60, 0),
+                  child: Wrap(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.person_outline, size: 22,color: Colors.grey,),
+
+                          Container(
+                            color: Colors.white,
+                            margin: EdgeInsets.fromLTRB(20, 3, 0, 3),
+                            padding: EdgeInsets.only(top: 3),
+                            child: Row( 
+                              children:[
+
+                              Text('Name',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: ssFont,
+                                color: Colors.grey,
+                              ),
+                            ),
+                             
+                            Padding(padding: EdgeInsets.only(left: 20),),
+
+                            //SWITCH
+                           Text(userdata.firstName+ " " +userdata.lastName,
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: ssFont,
+                                color: Colors.grey,
+                              ),
+                            )
+
+                            ],
+                          ),
+                          )
+                        ],
+                      ),
+
+                      SizedBox(
+                        height: 30,
+                      ),
+
+
+                      //PHONE ROW
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.phone, size: 22,color: Colors.grey,),
+
+                          Container(
+                            color: Colors.white,
+                            margin: EdgeInsets.fromLTRB(20, 3, 0, 3),
+                            padding: EdgeInsets.only(top: 3),
+                            child: Row( 
+                              children:[
+
+                              Text('Phone',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: ssFont,
+                                color: Colors.grey,
+                              ),
+                            ),
+                             
+                            Padding(padding: EdgeInsets.only(left: 20),),
+
+                           Text(userdata.phone, //SWITCH
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: ssFont,
+                                color: Colors.grey,
+                              ),
+                            )
+
+                            ],
+                          ),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(
+                        height: 30,
+                      ),
+
+                      //EMAIL ROW
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.email, size: 22,color: Colors.grey,),
+
+                          Container(
+                            color: Colors.white,
+                            margin: EdgeInsets.fromLTRB(20, 3, 0, 3),
+                            padding: EdgeInsets.only(top: 3),
+                            child: Row( 
+                              children:[
+
+                              Text('Email',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: ssFont,
+                                color: Colors.grey,
+                              ),
+                            ),
+                             
+                            Padding(padding: EdgeInsets.only(left: 20),),
+
+                           Text(userdata.email, //SWITCH
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: ssFont,
+                                color: Colors.grey,
+                              ),
+                            )
+
+                            ],
+                          ),
+                          ),
+                        ],
+                      ),
+
+                      SizedBox(
+                        height: 30,
+                      ),
+
+
+                      //ADDRESS
+                      Row(
+                        children: <Widget>[
+                          Icon(Icons.location_on, size: 22,color: Colors.grey,),
+
+                          Container(
+                            color: Colors.white,
+                            margin: EdgeInsets.fromLTRB(20, 3, 0, 3),
+                            padding: EdgeInsets.only(top: 3),
+                            child: Row( 
+                              children:[
+
+                              Text('Address',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: ssFont,
+                                color: Colors.grey,
+                              ),
+                            ),
+                             
+                            Padding(padding: EdgeInsets.only(left: 20),),
+
+                           Text(userdata.address, //SWITCH
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: ssFont,
+                                color: Colors.grey,
+                              ),
+                            )
+
+                            ],
+                          ),                          ),
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+
+
+                //PLACE ORDER BUTTON
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: EdgeInsets.fromLTRB(40, 15, 40, 0),
+                    padding: EdgeInsets.fromLTRB(0, 0.0, 0.0, 2.5),
+                    height: 28,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: Colors.grey,
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          offset: Offset.zero,
+                          color: Colors.grey[400],
+                          blurRadius: 11,
+                          spreadRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Text(
+                          'Save changes and continue',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: ssFont,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+
+
+
+
+
+              ],
+            ),
+
+
+          );
+        
+
+      
+    }
+  );
+  }
+}
